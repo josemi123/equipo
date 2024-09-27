f56ec699543f909606ff2352f644befd04993444
josemi123 <jgarred1806@g.educaand.es>
2024-09-27 18:33:06 +0200

    ampliacion

--- vamos.java
+++ vamos.java
@@ -2,5 +2,6 @@ public class vamos {
   public static void main(String[] args) {
     System.out.println("vamos pitu dood");
 	System.out.println("vamos mostro"); 
+		System.out.println("el año que viene eres nacional");
  }
 }
